0b5f9a43bf8947f9ba30dbb7e46c55e3927c6ff7
Charles Brown-Roberts <cgb37@miami.edu>
2015-10-19 21:22:12 -0400

    added jQuery full calendar plugin

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by charlesbrown-roberts on 10/19/15.
+ */
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});
