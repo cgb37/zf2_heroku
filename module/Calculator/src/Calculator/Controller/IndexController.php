7900f15d7c22acd69a383d03cb378da6dae4753d
Charles Brown-Roberts <cgb37@miami.edu>
2015-10-21 00:50:07 -0400

    removed test code from controller

--- module/Calculator/src/Calculator/Controller/IndexController.php
+++ module/Calculator/src/Calculator/Controller/IndexController.php
@@ -18,54 +18,12 @@ class IndexController extends AbstractActionController
 
         $objPC = $this->getPaydateCalculator();
 
-        $fund_day = "2015-10-05";
+        $fund_day = "2015-10-12";
         $period = $objPC->Calculate_Due_Date($fund_day);
 
         var_dump($period);
 
 
-
-        /*
-        //var_dump($objPC->setPayDays());
-        $period = $objPC->setPayDays();
-
-        $events = array();
-        foreach ($period as $date) {
-            $paydayTimestamp = strtotime( $date->format('d-m-Y'));
-
-            if($objPC->isHoliday($paydayTimestamp)) {
-
-                //echo $date->format('d-m-Y'). " is a holiday<br>";
-                $start = $date->format('Y-m-d');
-                $title = "Holiday";
-
-            } else {
-                $dateString = $date->format("Y-m-d");
-                //echo $dateString. " is Not a holiday<br>";
-
-                if($objPC->isWeekend($dateString)) {
-                    $title =  "weekend";
-                } else {
-                    $title = " Not a weekend<br>";
-                }
-
-                $start = $dateString;
-
-            }
-
-            $events[] = array(
-
-                'title' => $title,
-                'start' => $start
-
-            );
-
-
-
-        }
-
-        var_dump(json_encode($events));
-        */
         return new ViewModel();
     }
 
