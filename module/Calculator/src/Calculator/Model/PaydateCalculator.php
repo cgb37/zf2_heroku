e79324203b6f03f56b47252b61136b029b5ef640
Charles Brown-Roberts <cgb37@miami.edu>
2015-10-20 22:31:02 -0400

    paydate calculator model

--- module/Calculator/src/Calculator/Model/PaydateCalculator.php
+++ module/Calculator/src/Calculator/Model/PaydateCalculator.php
@@ -6,6 +6,21 @@
  * Time: 10:13 PM
  */
 
+/** This function determines the first available due date following the funding of a loan.
+ * The paydate will be at least 10 days in the future from the $fund_day. The
+ * due_date will fall on a day that is a paydate based on their paydate model * specified by '$pay_span' unless
+ * the date must be adjusted forward to miss a
+ * weekend or backward to miss a holiday
+ * Holiday adjustment takes precedence over Weekend.
+ *
+ * @param unix_timestamp $fund_day The day the loan was funded.
+ * @param array $holiday_array An array of unix timestamp's containing holidays.
+ * @param string $pay_span A string representing the frequency at which the customer is paid. (weekly,bi-weekly,monthly)
+ * @param unix_timestamp $pay_day A timestamp containing one of the customers paydays
+ * @param bool $direct_deposit A boolean determining whether or not the customer receives their paycheck via direct deposit.
+ * @return unix_timestamp A unix timestamp representing the determined due date.
+ */
+
 namespace Calculator\Model;
 
 
@@ -15,48 +30,200 @@ class PaydateCalculator
     protected $_timezone;
     protected $_fund_day;
     protected $_holidays = array("01-01", "19-01", "16-02", "25-05", "03-07", "07-09", "12-10", "11-11", "26-11", "25-12");
+    protected $_pay_span;
+    protected $_direct_deposit;
+    public $due_date;
 
     /**
      * Federal Holidays
      * https://www.opm.gov/policy-data-oversight/snow-dismissal-procedures/federal-holidays/
      *
-     * 01-01 Thursday,  January 1	 New Year’s Day
-     * 01-19 Monday,    January 19	 Birthday of Martin Luther King, Jr.
-     * 02-16 Monday,    February 16* Washington’s Birthday
-     * 05-25 Monday,    May 25	     Memorial Day
-     * 07-03 Friday,    July 3**	 Independence Day
-     * 09-07 Monday,    September 7	 Labor Day
-     * 10-12 Monday,    October 12	 Columbus Day
-     * 11-11 Wednesday, November 11	 Veterans Day
-     * 11-26 Thursday,  November 26	 Thanksgiving Day
-     * 12-25 Friday,    December 25	 Christmas Day
+     * 01-01 New Year’s Day
+     * 01-19 Birthday of Martin Luther King, Jr.
+     * 02-16 Washington’s Birthday
+     * 05-25 Memorial Day
+     * 07-03 Independence Day
+     * 09-07 Labor Day
+     * 10-12 Columbus Day
+     * 11-11 Veterans Day
+     * 11-26 Thanksgiving Day
+     * 12-25 Christmas Day
      */
 
 
     public function __construct()
     {
-        $this->setFundDay("2015-10-19");
+        //$this->setFundDay("2015-10-19");
         $this->setTimezone("America/New_York");
         $this->setHolidays($this->_holidays, null);
+        $this->setDirectDeposit("No");
+        $this->setPaySpan("monthly"); // weekly, bi-weekly, monthly
     }
 
-    /** This function determines the first available due date following the funding of a loan.
-     * The paydate will be at least 10 days in the future from the $fund_day. The
-     * due_date will fall on a day that is a paydate based on their paydate model * specified by '$pay_span' unless
-     * the date must be adjusted forward to miss a
-     * weekend or backward to miss a holiday
-     * Holiday adjustment takes precedence over Weekend.
-     *
-     * @param unix_timestamp $fund_day The day the loan was funded.
-     * @param array $holiday_array An array of unix timestamp's containing holidays.
-     * @param string $pay_span A string representing the frequency at which the customer is paid. (weekly,bi-weekly,monthly)
-     * @param unix_timestamp $pay_day A timestamp containing one of the customers paydays
-     * @param bool $direct_deposit A boolean determining whether or not the customer receives their paycheck via direct deposit.
-     * @return unix_timestamp A unix timestamp representing the determined due date.
-     */
-    public function Calculate_Due_Date($fund_day, $holiday_array, $pay_span, $pay_day, $direct_deposit)
+
+    //Calculate_Due_Date($fund_day, $holiday_array, $pay_span, $pay_day, $direct_deposit)
+    public function Calculate_Due_Date($fund_day)
     {
-        return 0;
+
+        $first_day = date('Y-m-d', strtotime($fund_day. ' + 9 days'));
+
+        $start = new \DateTime($first_day, $this->getTimezone());
+        $interval = new \DateInterval($this->getPaySpan());
+        //$end = new \DateTime('2015-12-31', $this->getTimezone());
+        $occurrences = 1;
+        $period = new \DatePeriod($start, $interval, $occurrences, \DatePeriod::EXCLUDE_START_DATE);
+
+        foreach($period as $dt){
+            $due_date = $dt->format("Y-m-d");
+
+            //is direct deposit?
+            if($this->getDirectDeposit() == true) {
+
+                //is it a weekend?
+                if($this->isWeekend($due_date)) {
+
+                    //is it saturday or sunday
+                    if($dt->format("N") == 6) {
+
+                        //saturday - add 2 days
+                        $due_date = date('Y-m-d', strtotime($due_date. ' + 2 days'));
+                        return " is a saturday - add 2 days: ". $due_date;
+
+                    } elseif($dt->format("N") == 7) {
+
+                        //sunday - add 1 day
+                        $due_date = date('Y-m-d', strtotime($due_date. ' + 1 days'));
+                        return " is a sunday - add 1 day: ". $due_date;
+
+                    } else {
+
+                        continue;
+
+                    }
+
+                } else {
+
+                    //is it a holiday?
+                    if($this->isHoliday(strtotime($due_date)) == true ) {
+
+                        //it is a holiday so subtract a day
+                        $due_date = date('Y-m-d', strtotime($due_date. ' - 1 days'));
+
+                        //is it a weekend?
+                        if($this->isWeekend($due_date)) {
+
+                            //is it saturday or sunday
+                            if($dt->format("N") == 6) {
+
+                                //saturday - add 2 days
+                                $due_date = date('Y-m-d', strtotime($due_date. ' + 2 days'));
+                                return " is a saturday - add 2 days: ". $due_date;
+
+                            } elseif($dt->format("N") == 7) {
+
+                                //sunday - add 1 day
+                                $due_date = date('Y-m-d', strtotime($due_date. ' + 1 days'));
+                                return " is a sunday - add 1 day: ". $due_date;
+
+                            } else {
+
+                                continue;
+
+                            }
+
+                        } else {
+
+                            return $due_date;
+
+                        }
+
+
+                    } else {
+
+                        return $due_date;
+
+                    }
+
+                }
+
+            } else {
+
+                // not direct deposit - add a day
+                $due_date = date('Y-m-d', strtotime($due_date. ' + 1 days'));
+
+                //is it a weekend?
+                if($this->isWeekend($due_date)) {
+
+                    //is it saturday or sunday
+                    if($dt->format("N") == 5) {
+
+                        //saturday - add 2 days
+                        $due_date = date('Y-m-d', strtotime($due_date. ' + 2 days'));
+                        return " Not DD - is a saturday - add 2 days: ". $due_date;
+
+                    } elseif($dt->format("N") == 6) {
+
+                        //sunday - add 1 day
+                        $due_date = date('Y-m-d', strtotime($due_date. ' + 1 days'));
+                        return "Not DD is a sunday - add 1 day: ". $due_date;
+
+                    } else {
+
+                        continue;
+
+                    }
+
+                } else {
+
+                    //is it a holiday?
+                    if($this->isHoliday(strtotime($due_date)) == true ) {
+
+                        //it is a holiday so subtract a day
+                        $due_date = date('Y-m-d', strtotime($due_date. ' - 1 days'));
+
+                        //is it a weekend?
+                        if($this->isWeekend($due_date)) {
+
+                            //is it saturday or sunday
+                            if($dt->format("N") == 5) {
+
+                                //saturday - add 2 days
+                                $due_date = date('Y-m-d', strtotime($due_date. ' + 1 days'));
+                                return "Not DD is a saturday - add 2 days: ". $due_date;
+
+                            } elseif($dt->format("N") == 6) {
+
+                                //sunday - add 1 day
+                                $due_date = date('Y-m-d', strtotime($due_date. ' + 1 days'));
+                                return "Not DD is a sunday - add 1 day: ". $due_date;
+
+                            } else {
+
+                                continue;
+
+                            }
+
+                        } else {
+
+                            return $due_date;
+
+                        }
+
+
+                    } else {
+
+                        return $due_date;
+
+                    }
+
+                }
+
+
+
+            }
+
+        }
+
     }
 
     public function getCurrentYear()
@@ -143,12 +310,75 @@ class PaydateCalculator
 
     public function setPayDays()
     {
-        $start = new \DateTime('2015-01-01');
-        $interval = new \DateInterval('P1W');
-        $end = new \DateTime('2015-12-31');
+
+        $start = new \DateTime('2015-01-11', $this->getTimezone());
+        $interval = new \DateInterval('P2W');
+        $end = new \DateTime('2015-12-31', $this->getTimezone());
 
         $period = new \DatePeriod($start, $interval, $end, \DatePeriod::EXCLUDE_START_DATE);
         return $period;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getPaySpan()
+    {
+        return $this->_pay_span;
+    }
+
+    /**
+     * @param mixed $pay_span
+     */
+    public function setPaySpan($span)
+    {
+        $span = strtolower($span);
+        switch($span) {
+
+            case 'weekly':
+                $pay_span = 'P1W';
+                break;
+            case 'bi-weekly':
+                $pay_span = 'P2W';
+                break;
+            case 'monthly':
+                $pay_span = 'P1M';
+                break;
+
+        }
+        $this->_pay_span = $pay_span;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDirectDeposit()
+    {
+        return $this->_direct_deposit;
+    }
+
+    /**
+     * @param mixed $direct_deposit
+     */
+    public function setDirectDeposit($option)
+    {
+        $option = strtolower($option);
+        switch($option) {
+
+            case 'No':
+                $direct_deposit = false;
+                break;
+            case 'Yes':
+                $direct_deposit = true;
+                break;
+            default:
+                $direct_deposit = true;
+        }
+
+        $this->_direct_deposit = $direct_deposit;
+    }
+
+
+
 
 }
